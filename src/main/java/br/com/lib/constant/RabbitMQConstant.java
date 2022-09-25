74e12862e65411f0cc06473deb3cd4268286d4d3
Leonardo Santana <leosantana9140@gmail.com>
2022-09-25 16:05:42 -0300

    Ajuste em constantes.

--- src/main/java/br/com/lib/constant/RabbitMQConstant.java
+++ src/main/java/br/com/lib/constant/RabbitMQConstant.java
@@ -1,6 +1,6 @@
 package br.com.lib.constant;
 
 public class RabbitMQConstant {
-    public static final String EXCHANGE_NAME = "amq.direct";
-    public static final String PRODUCT_QUEUE = "PRODUCT";
+    public static final String EXCHANGE_TYPE = "amq.direct";
+    public static final String PRODUCT_QUEUE_NAME = "PRODUCT";
 }
