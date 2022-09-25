3c40edded82f507146ad21129d536677f24812d9
Leonardo Santana <leosantana9140@gmail.com>
2022-09-25 16:02:08 -0300

    Primeira versão da biblioteca.

---
+++ src/main/java/br/com/lib/model/Product.java
@@ -0,0 +1,24 @@
+package br.com.lib.model;
+
+import java.io.Serializable;
+
+public class Product implements Serializable {
+    private String productId;
+    private double productPrice;
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+}
